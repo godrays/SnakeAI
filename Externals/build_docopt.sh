930c6ccefdd89116925551fcdd4dc4dc61c5575e
Arkin <arkin.terli@gmail.com>
2023-03-19 15:40:54 -0700

    Add docopt external library to parse command line parameters.

---
+++ Externals/build_docopt.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#
+#  Copyright (c) 2020-Present, Arkin Terli. All rights reserved.
+#
+
+lib_name=docopt
+lib_version=0.6.3
+lib_url=https://github.com/docopt/docopt.cpp.git
+
+pushd .
+
+git clone --recurse-submodules -b v$lib_version $lib_url ./$lib_name/$lib_version
+cd $lib_name/$lib_version
+
+# Patch URL
+cp ../../DocOptPatch.txt ./CMakeLists.txt
+
+rm -rf build
+rm -rf installed
+mkdir build
+cd build
+
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../installed
+cmake --build . --target install -- -j `nproc --all`
+
+cd ..
+rm -rf .git .gitattributes .github .gitignore build
+
+popd
+
