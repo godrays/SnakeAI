bb2d1b58d58509b2f4b0533960d44cf0f9c62f26
Arkin <arkin.terli@gmail.com>
2024-02-09 16:32:26 -0800

    Forgot to add the new build scripts.

---
+++ Externals/build_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./build_brotli.sh
+./build_bzip2.sh
+./build_docopt.sh
+./build_eigen.sh
+./build_freetype.sh
+./build_sfml.sh
+./build_zlib.sh
---
+++ Externals/build_brotli.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#
+#  Copyright (c) 2023-Present, Arkin Terli. All rights reserved.
+#
+
+lib_name=brotli
+lib_version=1.1.0
+lib_url=https://github.com/google/brotli.git
+
+pushd .
+
+rm -rf $lib_name
+
+git clone --recurse-submodules -b v$lib_version $lib_url ./$lib_name/$lib_version
+cd $lib_name/$lib_version
+
+rm -rf build
+mkdir build
+cd build
+
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=FALSE -DCMAKE_INSTALL_PREFIX=../installed
+cmake --build . --target install -- -j `nproc --all`
+
+cd ..
+rm -rf .git .gitattributes .github .gitignore build
+
+popd
